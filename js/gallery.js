381f8d43484f4aeef2cf2988f6180193963dcb48
Vali Bojici <valibojici@gmail.com>
2021-04-13 21:21:36 +0300

    update

--- js/gallery.js
+++ js/gallery.js
@@ -1,13 +1,61 @@
-const img_container = document.getElementById('gallery-inner-container');
-console.log(img_container);
-const img_top = document.getElementById('gallery-img-top');
-const img_bot = document.getElementById('gallery-img-bottom');
-const slide1 = document.getElementById('slide');
+const gal_img_container = document.getElementById('gallery-img-container');
+const leftBtn = document.getElementById('gallery-button-left');
+const rightBtn = document.getElementById('gallery-button-right');
+const imgWidth = gal_img_container.getBoundingClientRect().width;
 
-img_container.addEventListener('click', event =>{
-    img_bot.style.position = 'relative';
-    img_top.style.position = 'relative';
-    img_top.style.left = (img_top.style.left - 500) + 'px';
-    img_bot.style.left = (img_bot.style.left + 500) + 'px';
-    console.log(event.target)
+
+
+let imgs = []
+let imgIndex = 0;
+
+async function slide(pixels){
+    gal_img_container.style.transform = 'translateX(' + pixels + 'px)';
+}
+
+
+for(let i = 1;i <= 6; ++i){
+    let img = document.createElement('img');
+    img.setAttribute('src','../img/photos/photo' + i + '.png');
+    img.classList.add('gallery-img');
+
+    imgs.push(img);
+}
+
+let firstImg = imgs[0];
+let lastImg = imgs[imgs.length-1];
+imgs.push(firstImg);
+imgs.unshift(lastImg);
+
+
+console.log(imgs);
+
+for(let img of imgs){
+    gal_img_container.appendChild(img.cloneNode(true));
+}
+ 
+imgs = document.querySelectorAll('.gallery-img');
+
+rightBtn.addEventListener('click', event =>{
+    
+    imgIndex++;
+    gal_img_container.style.transition = 'transform 0.4s';
+        slide(-imgIndex*imgWidth);
+
+    console.log(imgIndex-1, imgIndex);
+    if(imgIndex == imgs.length-1)
+    {
+        imgIndex = 1;
+        gal_img_container.style.transition = 'none';
+        slide(-imgIndex*imgWidth);
+        //gal_img_container.style.transition = 'transform 0.4s';
+    }
+    
+    
+});
+
+leftBtn.addEventListener('click', event =>{
+    gal_img_container.style.transition = 'transform 0.4s';
+    imgIndex--;
+    if(imgIndex == -1)imgIndex = imgs.length - 1;
+    slide(-imgIndex*imgWidth);
 });
